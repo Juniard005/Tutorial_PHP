a8acb1ba64af9b2e1233428c84bde0835dbfb18a
Juniard005 <jardi131gmail.com>
2025-06-19 01:16:07 +0700

    kita ulang lagi dari awal sampai berhasil

---
+++ Pengenalan/Program_Hello_World/Testing.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Hello, World!";
+
+// ini cuma pengenalan doang
+// tidak ada yang perlu diubah
+// ini adalah contoh program PHP yang sangat sederhana
+// untuk menampilkan teks "Hello, World!" ke layar
+
+?>
