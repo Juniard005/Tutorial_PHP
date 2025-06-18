47e1b8391df627626653c34608bbf59ad772d918
Juniard005 <jardi131gmail.com>
2025-06-19 01:41:05 +0700

    ngantuk gue mau tidur dulu

---
+++ Pengenalan/Variabel/Variabel.php
@@ -0,0 +1,15 @@
+<?php
+
+# Ini adalah Namaku 
+$aku = "Ardi Juni Yansyah";
+echo "Nama saya adalah $aku";
+
+# ini adalah Nama Istriku
+$istri = "Sumire Kakei";
+echo "Nama istriku adalah $istri";
+
+# ini adalah Nama Anakku
+$putriku = "Karina Amani Fajriyah";
+echo "Nama putri kesayanganku adalah $putriku dan Aku sangat menyayanginya";
+
+?>
